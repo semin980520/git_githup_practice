1240a55a8f2e8d4ce9e1be8beb562771dd9e1153
semin980520 <tpalsdl87@naver.com>
2025-12-02 17:14:21 +0900

    rlapwe

--- git_syntax/2.basic_command.sh
+++ git_syntax/2.basic_command.sh
@@ -5,3 +5,18 @@ git status
 git add .
 # 특정파일만 add할 경우 (경로까지 포함)
 git add test_folder/test1.txt
+# commit을 통해 메시지타이틀과 메세지내용을 커밋이력으로 생성
+git commit -m "메세지제목" -m "메세지내용"
+# git commit만 이볅하고 엔터시 vi 모드 -> 첫줄:타이틀, 두번째줄부터 내용 -> :wq 저장후 종료시 커밋ID생성
+git commit
+
+# commit 이력 확인
+git log
+git log --oneline
+# head하단의 로그만 보이는것이 아니라, 모든 commit이력을 조회
+# head : 현재 사용(checkout)하고 있는 commitID를 의미 
+git log --all 
+# 원격저장소로 업로드 
+git push origin 브랜치명
+# 충돌발생시 충돌무시하고, 로컬기준의 코드를 원격에 덮어쓰기(절대 사용하지 말것)
+git push origin 브랜치명 --force
