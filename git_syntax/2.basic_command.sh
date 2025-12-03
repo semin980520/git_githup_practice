ae916979f4f3a1dfb99f213d52482295cf06b518
semin980520 <tpalsdl87@naver.com>
2025-12-03 11:25:16 +0900

    Revert "취소테스트"
    This reverts commit bc71d5e3212aaa24783b7ad2592e587598436f3b.

--- git_syntax/2.basic_command.sh
+++ git_syntax/2.basic_command.sh
@@ -24,10 +24,3 @@ git push origin 브랜치명 --force
 git checkout 커밋ID
 # 특전 branch로의 전환
 git checkout 브랜치명
-# pull은 원격사항을 local로 내려받는것(fetch+merge)
-git pull origin main
-# fetch는 변경사항을 local로 가져오되, 병합을 하지 않는것.
-git fetch origin main
-# commitID간 또는 브랜치사이의 비교 명령어
-git diff commitId1 commitId2
-git diff 브랜치1 브랜치2
